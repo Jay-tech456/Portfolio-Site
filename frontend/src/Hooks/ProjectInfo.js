47b262d105391ee38ca2e5e55f81547e1e380233
Manjesh Prasad <manjeshprasad@Manjeshs-MacBook-Air.local>
2024-07-09 16:58:24 -0700

    Fixed the hook of the Project Page

---
+++ frontend/src/Hooks/ProjectInfo.js
@@ -0,0 +1,31 @@
+import OpenD from "../Components/Images/OpenD.png";
+
+const ProjectsInfo = [
+    {
+        title: 'OpenD - Modern Web 3.0 E-Commerce Market',
+        imageUrl: OpenD,
+        description: 'Engineered the creation of a decentralized NFT Marketplace e-commerce replica, empowering users to seamlessly mint, list, and trade Non-Fungible Tokens (NFTs) on the ICP network, revolutionizing the digital asset landscape. Achieved remarkable results, including a consistent transaction processing time and a staggering 75% increase in UI/UX, highlighting the transformative on the digital marketplace ecosystem.', 
+        toolStack: ['Motoko', 'Node.js', 'JavaScript', 'Blockchain', 'Docker', 'React']
+    },
+    {
+        title: 'Decentralized Google Keeper App',
+        imageUrl: 'https://github.com/Jay-tech456/Decentralized-Google-Keeper-App/blob/main/dkeeper.png?raw=true',
+        description: ['A decentralized Google Keep app securely manages your notes using cutting-edge Web 3.0 blockchain technology, such as the Internet Computer Protocol.', 
+                        'By merging decentralized Internet Computer Protocol principles with seamless user experiences, it efficiently handles various Kanban tasks.'],
+        toolStack: ['TypeScript', 'ICP', 'React', 'Motoko', 'HTML5/CSS3', 'Blockchain']
+    },
+    {
+        title: 'CryptoToken - OAT',
+        imageUrl: 'https://github.com/Jay-tech456/Blockchain-Token-OAT/blob/main/demoImage.png?raw=true',
+        description: ['Developed a decentralized blockchain token for a DeFi app, enabling users to transfer, claim, and trade digital assets. Achieved 99.9% uptime with an average transaction confirmation time of 2 seconds, capable of serving over 10,000 active users.'], 
+        toolStack: ['JavaScript', 'React', 'Motoko', 'HTML5/CSS3', 'Blockchain']
+    },
+    {
+        title: 'Foodie Review Web App',
+        imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fgithub.com%2Fhemachandarn%2FYelp-Dataset&psig=AOvVaw028VYEn4Vhax-F5uQCmhF6&ust=1720653703010000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCID7wdeMm4cDFQAAAAAdAAAAABAE',
+        description: 'Deployed cloud infrastructure (MongoDB Atlas) to streamline database operations, ensuring high performance and availability while reducing operational complexities associated with local databases; leading to a uniform collaboration .', 
+        toolStack: ['MongoDB Atlas', 'Express', 'React.js', 'Node.js', 'Postman', 'Figma', 'Docker']
+    }
+];
+
+export default ProjectsInfo;
